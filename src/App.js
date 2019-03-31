f0556972a07d07fd3af20c46093a1110f99195fd
Michael Kovzanovich <mikhail.kovzanovich@corp.mail.ru>
2019-03-31 17:22:36 +0300

    fix: draw new rect when d&d

--- src/App.js
+++ src/App.js
@@ -21,10 +21,15 @@ class App extends Component {
       height: window.innerHeight
     });
 
-
     this.layer = new Konva.Layer();
 
     this.stage.on('mousedown', (e) => {
+      console.log(e.target);
+
+      if(e.target.attrs.name === 'rect') {
+        return
+      }
+
       this.setState({
         isDrawing: true,
         startDrawingPositionX: e.evt.pageX,
@@ -34,7 +39,7 @@ class App extends Component {
       this.group = new Konva.Group({
         x: e.evt.pageX,
         y: e.evt.pageY,
-        // draggable: true
+        draggable: true
       });
 
       this.rect = new Konva.Rect({
@@ -47,7 +52,8 @@ class App extends Component {
         position: 'relative',
         overflow: 'hidden',
         cursor: 'pointer',
-        strokeWidth: 2
+        strokeWidth: 2,
+        name: 'rect'
       });
 
       this.group.add(this.rect);
@@ -56,6 +62,8 @@ class App extends Component {
     });
 
     this.stage.on('mousemove', (e) => {
+      e.cancelBubble = true;
+
       if(!this.state.isDrawing) {
         return
       }
@@ -76,95 +84,9 @@ class App extends Component {
     this.stage.add(this.layer);
   }
 
-  // handleMouseDown = (e) => {
-  //   e.persist();
-  //
-  //   this.setState({
-  //     isDrawing: true,
-  //     startDrawingPositionX: e.pageX,
-  //     startDrawingPositionY: e.pageY,
-  //   });
-  //
-  //   this.group = new Konva.Group({
-  //     x: e.pageX,
-  //     y: e.pageY,
-  //     draggable: true
-  //   });
-  //
-  //   this.rect = new Konva.Rect({
-  //     x: e.pageX,
-  //     y: e.pageY,
-  //     width: 0,
-  //     height: 0,
-  //     fill: 'rgba(0, 0, 0, 0.3)',
-  //     stroke: 'black',
-  //     position: 'relative',
-  //     overflow: 'hidden',
-  //     cursor: 'pointer',
-  //     strokeWidth: 4
-  //   });
-  //
-  //   this.group.add(this.rect);
-  //   this.layer.add(this.group);
-  //   this.group.draw();
-  //
-  // };
-  //
-  // handleMouseMove = (e) => {
-  //   if(!this.state.isDrawing) {
-  //     return
-  //   }
-  //   e.persist();
-  //   e.stopPropagation();
-  //
-  //   this.rect.attrs.width = e.pageX - this.state.startDrawingPositionX;
-  //   this.rect.attrs.height = e.pageY - this.state.startDrawingPositionY;
-  //
-  //   this.layer.draw();
-  //
-  // };
-
-  // handleMouseUp = () => {
-  //   const crossImageObj = new Image();
-  //   crossImageObj.onload = () => {
-  //     const cross = new Konva.Image({
-  //       image: crossImageObj,
-  //       x: 20,
-  //       y: 20,
-  //       width: 50,
-  //       height: 50,
-  //     });
-  //
-  //     cross.on('click', () => {
-  //       alert('click');
-  //       this.stage.remove(this.group);
-  //     });
-  //
-  //     /* adding images to layer */
-  //     this.group.add(cross);
-  //     this.layer.draw();
-  //   };
-  //
-  //   crossImageObj.src = 'https://pp.userapi.com/c845018/v845018528/121318/eaGeeQDWigE.jpg';
-  //
-  //   this.setState({
-  //     isDrawing: false,
-  //     startDrawingPositionX: null,
-  //     startDrawingPositionY: null,
-  //   })
-  // };
-
   render() {
-
-
     return (
-        <div id='container'
-             // onMouseDown={ (e) => this.handleMouseDown(e) }
-             // onMouseMove={ this.handleMouseMove }
-             // onMouseUp={ this.handleMouseUp }
-        >
-
-        </div>
+        <div id='container' />
     );
   }
 }
